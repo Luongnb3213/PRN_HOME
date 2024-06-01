cb06c85a85fab47834f22e6f8e4a9a0b57525512
Le Son <lson999920.work@gmail.com>
2024-06-01 14:24:57 +0700

    Create login, forgot, signup screens

---
+++ Pages/Authentication/ForgotPassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_Assignment.Pages.Authentication
+{
+    public class ForgotPasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
