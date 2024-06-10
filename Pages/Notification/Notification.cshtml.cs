3f0a2d90e8b97f19306e9352cf09574cd9af8709
Le Son <lson999920.work@gmail.com>
2024-06-10 09:03:23 +0700

    add noti page + followers page

---
+++ Pages/Notification/Notification.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_Assignment.Pages.Notification
+{
+    public class NotificationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
