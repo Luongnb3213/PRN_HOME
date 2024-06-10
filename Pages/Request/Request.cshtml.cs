a5860f669b4294b5301ba109eb4b8c1cb2a5087d
Le Son <lson999920.work@gmail.com>
2024-06-10 10:20:30 +0700

    finish request page

---
+++ Pages/Request/Request.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_Assignment.Pages.Request
+{
+    public class RequestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
