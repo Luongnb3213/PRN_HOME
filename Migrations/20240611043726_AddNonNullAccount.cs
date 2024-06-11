84663562c1d55b2454e7f80d00763392a942a3c6
Luongnb3213 <luongnb3213@gmail.com>
2024-06-11 15:45:14 +0700

    UPDATED update database v3

---
+++ Migrations/20240611043726_AddNonNullAccount.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PRN221_Assignment.Migrations
+{
+    public partial class AddNonNullAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isActive",
+                table: "Accounts",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Status",
+                table: "Accounts",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isActive",
+                table: "Accounts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Status",
+                table: "Accounts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- Models/Account.cs
+++ Models/Account.cs
@@ -6,17 +6,25 @@ namespace PRN221_Assignment.Models
 {
     public class Account
     {
+        public Account()
+        {
+            Follows = new HashSet<Follow>();
+            BLocks = new HashSet<Block>();
+            Threads = new HashSet<Thread>();
+            Comments = new HashSet<Comment>();
+
+        }
         [Key]
         public int UserID { get; set; }
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
-        public string Password { get; set; }
+        public string? Password { get; set; }
 
-        public bool Status { get; set; }
+        public bool? Status { get; set; }
 
-        public bool isActive { get; set; }
+        public bool? isActive { get; set; }
 
-        public virtual Info Info { get; set; }
+        public virtual Info? Info { get; set; }
         public virtual ICollection<Follow> Follows { get; set; }
         public virtual ICollection<Block> BLocks { get; set; }
 
