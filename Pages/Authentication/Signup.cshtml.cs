a2b6214046b2e0d07ef588fed23033e079b3c1f1
hoangnxhe171096 <hoangnxhe171096@fpt.edu.vn>
2024-06-12 13:09:03 +0700

    sign up

--- Pages/Authentication/Signup.cshtml.cs
+++ Pages/Authentication/Signup.cshtml.cs
@@ -1,12 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using PRN221_Assignment.Models;
 namespace PRN221_Assignment.Pages.Authentication
 {
     public class SignupModel : PageModel
     {
+
+        private readonly PRN221_Assignment.Respository.DataContext _context;
+
+        public SignupModel(PRN221_Assignment.Respository.DataContext context)
+        {
+            _context = context;
+
+        }
+
+
         public void OnGet()
         {
         }
+
+
+        [BindProperty]
+        public Account? Account { get; set; }
+
+        public IActionResult OnPost()
+        {
+            string confirmPassword = Request.Form["confirmPassword"];
+            if (Account != null)
+            {
+                if (confirmPassword != Account.Password)
+                {
+                    return Page();
+                }
+                _context.Accounts.Add(Account);
+                _context.SaveChanges();
+                return RedirectToAction("/Authentication/Login");
+            }
+
+
+
+            return Page();
+
+        }
     }
+
 }
+
+
+
