9e5bf75f162e043360569761f93685440c86e068
hoangnxhe171096 <hoangnxhe171096@fpt.edu.vn>
2024-06-13 23:47:14 +0700

    ﻿Sign up Lastest Version

--- Pages/Authentication/Signup.cshtml.cs
+++ Pages/Authentication/Signup.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using PRN221_Assignment.Models;
 namespace PRN221_Assignment.Pages.Authentication
 {
@@ -25,63 +26,96 @@ namespace PRN221_Assignment.Pages.Authentication
 
         public IActionResult OnPost()
         {
-            Account acc = new Account {
+
+            string confirmPassword = Request.Form["confirmPassword"];
+
+
+            List<Account> accounts = _context.Accounts.Include(x => x.Info).ToList();
+           
+
+            accInfo accInfo = new accInfo
+            {
+                Name = Account.Info.Name,
+                UserName = Account.Info.userName,
+                Dob = Account.Info.Dob,
                 Email = Account.Email,
-                Password = Account.Password,
-                Status = true,
-            isActive = true,
+                Password = Account.Password
+
             };
 
-            
-            
-            
-            string confirmPassword = Request.Form["confirmPassword"];
-            if (acc != null)
+            if (confirmPassword != accInfo.Password)
             {
-                if (confirmPassword != acc.Password)
+
+                ModelState.AddModelError("", "Confirm password was wrong.");
+                return Page();
+            }
+
+            
+
+            foreach (var account in accounts)
+            {
+                if (accInfo.Email == account.Email)
                 {
+                    ModelState.AddModelError("Email", "This email already exists in the system.");
+                    return Page();
+                }
+                if (accInfo.UserName == account.Info?.userName)
+                {
+                    ModelState.AddModelError("userName", "This userName already exists in the system.");
                     return Page();
                 }
 
-                //foreach (var account in listAcc)
-                //{
-                //    if (acc.Email == account.Email)
-                //    {
-                //        return Page();
-                //    }
-                //}
-
-
-
-               
-
-                var demo = _context.Accounts.Add(acc);
-                
-                
-                _context.SaveChanges();
-
-                Info info = new Info
-                {
-
-                    userName = Account.Info.userName,
-                    Name = Account.Info.Name,
-                    Story = String.Empty,
-                    Dob = Account.Info.Dob,
-                    Image = String.Empty,
-
-                };
-
-
-                _context.Info.Add(info);
-                _context.SaveChanges();
-                return RedirectToPage("/Authentication/Login");
             }
 
+            Account acc = new Account
+            {
+                Email = accInfo.Email,
+                Password = accInfo.Password,
+                isActive = true,
+                Status = true
+            };
 
+            if (acc == null) {
+                return Page();
+            }
+            _context.Accounts.Add(acc);
+            _context.SaveChanges();
 
-            return Page();
+            Info info = new Info
+            {
+                UserID = acc.UserID,
+                Dob = accInfo.Dob,
+                Account = acc,
+                Image = String.Empty,
+                Name = accInfo.Name,
+                Story = "",
+                userName = accInfo.UserName,
+
+            };
+            if (info == null)
+            {
+                return Page();
+
+            }
+            _context.Info.Add(info);
+            _context.SaveChanges();
+            return RedirectToPage("/Authentication/Login");
+        }
+
+        class accInfo
+        {
+            public string UserName;
+
+            public string Name;
+
+            public string Email;
+
+            public string Password;
+            public DateTime Dob;
 
         }
+
+
     }
 
 }
