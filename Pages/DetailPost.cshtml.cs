d42c451ac4eeb4347fc85ec03616b4ec0295a29a
Le Son <lson999920.work@gmail.com>
2024-06-14 23:57:19 +0700

    working comment

--- Pages/DetailPost.cshtml.cs
+++ Pages/DetailPost.cshtml.cs
@@ -1,12 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PRN221_Assignment.Models;
 
 namespace PRN221_Assignment.Pages.Profile
 {
     public class DetailPostModel : PageModel
     {
+        public readonly PRN221_Assignment.Respository.DataContext context;
+        public DetailPostModel(PRN221_Assignment.Respository.DataContext _context)
+        {
+            context = _context;
+        }
+        [BindProperty(SupportsGet = true)]
+        public int ThreadId { get; set; }
+        [BindProperty]
+        public string Comment { get; set; }
+        public Models.Thread SelectedThread { get; set; }
+        public int numOfComment { get; set; }
+        public List<ThreadComment> listOriginalComment { get;set; }
         public void OnGet()
         {
+            SelectedThread = context.Thread
+                .Include(x => x.Account)
+                .ThenInclude(x => x.Info)
+                .Include(x => x.ThreadImages)
+                .Include(x => x.ThreadComments)
+                .Include(x => x.Comments)
+                .Include(x => x.Account.Info)
+                .FirstOrDefault(x => x.ThreadId == ThreadId);
+
+            numOfComment = context.ThreadComment.Count(x => x.ThreadId == ThreadId);
+
+            listOriginalComment = context.ThreadComment.Where(x => x.ThreadId == ThreadId)
+                .Include(x => x.Comment)
+                .ThenInclude(x => x.CommentImages)
+                .Include(x => x.Conversations)
+                .ToList();
+
+        }
+        public IActionResult OnPost()
+        {
+            Comment newComment = new Comment();
+            newComment.Content = Comment;
+            newComment.React = 0;
+            newComment.AuthorId = 1;
+            newComment.CreatedAt = DateTime.Now;
+            context.Comment.Add(newComment);
+            context.SaveChanges();
+
+            ThreadComment newThreadComment = new ThreadComment();
+            newThreadComment.CommentId = newComment.CommentId;
+            newThreadComment.ThreadId = ThreadId;
+            context.ThreadComment.Add(newThreadComment);
+            context.SaveChanges();
+            return RedirectToPage("/detailpost", new { threadId = ThreadId });
         }
     }
 }
--- wwwroot/js/profile/detailpost.js
+++ wwwroot/js/profile/detailpost.js
@@ -37,3 +37,35 @@ class threadDetail extends PopupBase {
 }
 customElements.define('thread-detail', threadDetail);
 
+document.addEventListener('DOMContentLoaded', function () {
+    function timeSince(date) {
+        const now = new Date();
+        const seconds = Math.floor((now - new Date(date)) / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
+        const months = Math.floor(days / 30);
+        const years = Math.floor(days / 365);
+
+        if (years >= 1) return years + (years > 1 ? " years" : " year");
+        if (months >= 1) return months + (months > 1 ? " months" : " month");
+        if (days >= 1) return days + (days > 1 ? " days" : " day");
+        if (hours >= 1) return hours + (hours > 1 ? " hours" : " hour");
+        if (minutes >= 1) return minutes + (minutes > 1 ? " minutes" : " minute");
+        return seconds + (seconds > 1 ? " seconds" : " second");
+    }
+
+    const timeElement = document.querySelector('.time');
+    const submitDate = timeElement.getAttribute('data-submit-date');
+    timeElement.textContent = timeSince(submitDate);
+
+    var timeComments = document.querySelectorAll('.author-comment-time');
+    timeComments.forEach((element) => {
+        const submitDate = element.getAttribute('data-submit-date');
+        element.textContent = timeSince(submitDate);
+    })
+});
+
+document.querySelector('.write-comment-btn-submit').addEventListener('click', function () {
+    document.querySelector('#commentForm').submit();
+});
