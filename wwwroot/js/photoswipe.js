dc410900426081c26ad66ed42622007ca554ee45
Luongnb3213 <luongnb3213@gmail.com>
2024-06-18 00:32:28 +0700

    UPdated - light-box

---
+++ wwwroot/js/photoswipe.js
@@ -0,0 +1,136 @@
+import PhotoSwipeLightbox from '/js/lib/photoswipe-lightbox.esm.js';
+
+
+class customImage extends HTMLElement {
+    constructor() {
+        super();
+        this.element = this.querySelector("img") || this.querySelector("video");
+        this.a = this.querySelector("a")
+        this.init();
+    }
+    init() {
+
+        var naturalHeight = parseFloat(this.element.naturalHeight);
+        var naturalWidth = parseFloat(this.element.naturalWidth);
+        var aspect_ratio = naturalWidth / naturalHeight
+        this.style = `--aspect-ratio :  ${aspect_ratio}`
+        var fixedHeight = 1500; 
+        var calculatedWidth = fixedHeight * aspect_ratio;
+
+        this.a.setAttribute("data-pswp-width", calculatedWidth);
+        this.a.setAttribute("data-pswp-height", fixedHeight);
+    }
+}
+customElements.define("custom-media", customImage);
+class SlideSection extends HTMLElement {
+    constructor() {
+        super();
+        this.globalSlide = null;
+        this.thumbnailSlide = null;
+        this.lightbox = null
+        this.id = this.getAttribute("id")
+        this.init();
+    }
+    init() {
+        this.initSlide();
+        this.initLightBox();
+
+    }
+
+    initSlide() {
+        const _this = this;
+        var autoplaying = _this?.dataset.autoplay === "true";
+        const loop = _this?.dataset.loop === "true";
+        const itemDesktop = _this?.dataset.desktop ? _this?.dataset.desktop : 4;
+        const itemTablet = _this?.dataset.tablet ? _this?.dataset.tablet : 2;
+        const itemMobile = _this?.dataset.mobile ? _this?.dataset.mobile : 1;
+        const direction = _this?.dataset.direction
+            ? _this?.dataset.direction
+            : "horizontal";
+        var autoplaySpeed = _this?.dataset.autoplaySpeed
+            ? _this?.dataset.autoplaySpeed * 1000
+            : 3000;
+        var speed = _this?.dataset.speed ? _this?.dataset.speed : 400;
+        const effect = _this?.dataset.effect ? _this?.dataset.effect : "slide";
+        const row = _this?.dataset.row ? _this?.dataset.row : 1;
+        var spacing = _this?.dataset.spacing ? _this?.dataset.spacing : 30;
+        const progressbar = _this?.dataset.paginationProgressbar === "true";
+        const autoItem = _this?.dataset.itemMobile === "true";
+        const arrowCenterimage = _this?.dataset.arrowCenterimage
+            ? _this?.dataset.arrowCenterimage
+            : 0;
+        spacing = Number(spacing);
+        autoplaySpeed = Number(autoplaySpeed);
+        speed = Number(speed);
+        if (autoplaying) {
+            autoplaying = { delay: autoplaySpeed };
+        }
+        if (direction == "vertical") {
+            _this.style.maxHeight = _this.offsetHeight + "px";
+        }
+        console.log(itemDesktop)
+        this.globalSlide = new Swiper(_this, {
+            slidesPerView: autoItem ? "auto" : itemMobile,
+            spaceBetween: spacing >= 15 ? 15 : spacing,
+            autoplay: autoplaying,
+            direction: direction,
+            loop: loop,
+            effect: effect,
+            speed: speed,
+            watchSlidesProgress: true,
+            watchSlidesVisibility: true,
+            grid: {
+                rows: row,
+                fill: "row",
+            },
+            navigation: {
+                nextEl: _this.querySelector(".swiper-button-next"),
+                prevEl: _this.querySelector(".swiper-button-prev"),
+            },
+            pagination: {
+                clickable: true,
+                el: _this.querySelector(".swiper-pagination"),
+                type: progressbar ? "progressbar" : "bullets",
+            },
+            breakpoints: {
+                768: {
+                    slidesPerView: itemTablet,
+                    spaceBetween: spacing >= 30 ? 30 : spacing,
+                },
+                1025: {
+                    slidesPerView: itemDesktop,
+                    spaceBetween: spacing,
+                },
+            },
+            thumbs: {
+                swiper: this.thumbnailSlide ? this.thumbnailSlide : null,
+            },
+
+            on: {
+
+            },
+        });
+    }
+    initLightBox() {
+        this.lightbox = new PhotoSwipeLightbox({
+            // may select multiple "galleries"
+            gallery: `#gallery--getting-started-${this.id}`,
+
+            // Elements within gallery (slides)
+            children: 'a',
+
+            // setup PhotoSwipe Core dynamic import
+            pswpModule: () => import('/js/lib/photoswipe.esm.js'),
+            wheelToZoom: true,
+            errorMsg: 'The photo cannot be loaded',
+            closeTitle: 'Close the dialog',
+            zoomTitle: 'Zoom the photo',
+            arrowPrevTitle: 'Go to the previous photo',
+            arrowNextTitle: 'Go to the next photo',
+            indexIndicatorSep: ' of '
+        });
+        this.lightbox.init();
+    }
+
+}
+customElements.define("slide-section", SlideSection);
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -27,23 +27,23 @@ class threadMain extends HTMLElement {
         })
     }
     saveScroll() {
-        var _this = this;
-        let aLink = _this.querySelector('.link-detail');
-        aLink.addEventListener('click', function () {
-            let scrollX = window.pageXOffset;
-            let scrollY = window.pageYOffset;
-            let currentScroll = {
-                scrollX,
-                scrollY
-            }
-            console.log(currentScroll);
-            //setTimeout(() => {
-            //    // Chuyển hướng đến href của thẻ <a>
-            //    window.location.href = e.target.href;
-            //}, 100);
-            sessionStorage.setItem('currentScroll', JSON.stringify(currentScroll));
-            sessionStorage.setItem('previous', '/');
-        })
+    //    var _this = this;
+    //    let aLink = _this.querySelector('.link-detail');
+    //    aLink.addEventListener('click', function () {
+    //        let scrollX = window.pageXOffset;
+    //        let scrollY = window.pageYOffset;
+    //        let currentScroll = {
+    //            scrollX,
+    //            scrollY
+    //        }
+    //        console.log(currentScroll);
+    //        //setTimeout(() => {
+    //        //    // Chuyển hướng đến href của thẻ <a>
+    //        //    window.location.href = e.target.href;
+    //        //}, 100);
+    //        sessionStorage.setItem('currentScroll', JSON.stringify(currentScroll));
+    //        sessionStorage.setItem('previous', '/');
+    //    })
     }
 }  
 customElements.define("thread-main", threadMain)
@@ -109,95 +109,7 @@ class popupCreate extends PopupBase {
 
 }
 customElements.define("popup-create", popupCreate)
-class SlideSection extends HTMLElement {
-    constructor() {
-        super();
-        this.globalSlide = null;
-        this.thumbnailSlide = null;
-        this.init();
-    }
-    init() {
-        this.initSlide();
-    }
 
-    initSlide() {
-        const _this = this;
-        var autoplaying = _this?.dataset.autoplay === "true";
-        const loop = _this?.dataset.loop === "true";
-        const itemDesktop = _this?.dataset.desktop ? _this?.dataset.desktop : 4;
-        const itemTablet = _this?.dataset.tablet ? _this?.dataset.tablet : 2;
-        const itemMobile = _this?.dataset.mobile ? _this?.dataset.mobile : 1;
-        const direction = _this?.dataset.direction
-            ? _this?.dataset.direction
-            : "horizontal";
-        var autoplaySpeed = _this?.dataset.autoplaySpeed
-            ? _this?.dataset.autoplaySpeed * 1000
-            : 3000;
-        var speed = _this?.dataset.speed ? _this?.dataset.speed : 400;
-        const effect = _this?.dataset.effect ? _this?.dataset.effect : "slide";
-        const row = _this?.dataset.row ? _this?.dataset.row : 1;
-        var spacing = _this?.dataset.spacing ? _this?.dataset.spacing : 30;
-        const progressbar = _this?.dataset.paginationProgressbar === "true";
-        const autoItem = _this?.dataset.itemMobile === "true";
-        const arrowCenterimage = _this?.dataset.arrowCenterimage
-            ? _this?.dataset.arrowCenterimage
-            : 0;
-        spacing = Number(spacing);
-        autoplaySpeed = Number(autoplaySpeed);
-        speed = Number(speed);
-        if (autoplaying) {
-            autoplaying = { delay: autoplaySpeed };
-        }
-        if (direction == "vertical") {
-            _this.style.maxHeight = _this.offsetHeight + "px";
-        }
-        console.log(itemDesktop)
-        this.globalSlide = new Swiper(_this, {
-            slidesPerView: autoItem ? "auto" : itemMobile,
-            spaceBetween: spacing >= 15 ? 15 : spacing,
-            autoplay: autoplaying,
-            direction: direction,
-            loop: loop,
-            effect: effect,
-            speed: speed,
-            watchSlidesProgress: true,
-            watchSlidesVisibility: true,
-            grid: {
-                rows: row,
-                fill: "row",
-            },
-            navigation: {
-                nextEl: _this.querySelector(".swiper-button-next"),
-                prevEl: _this.querySelector(".swiper-button-prev"),
-            },
-            pagination: {
-                clickable: true,
-                el: _this.querySelector(".swiper-pagination"),
-                type: progressbar ? "progressbar" : "bullets",
-            },
-            breakpoints: {
-                768: {
-                    slidesPerView: itemTablet,
-                    spaceBetween: spacing >= 30 ? 30 : spacing,
-                },
-                1025: {
-                    slidesPerView: itemDesktop,
-                    spaceBetween: spacing,
-                },
-            },
-            thumbs: {
-                swiper: this.thumbnailSlide ? this.thumbnailSlide : null,
-            },
-
-            on: {
-               
-            },
-        });
-    }
-
-    
-}
-customElements.define("slide-section", SlideSection);
 
 class CustomTextArea extends HTMLTextAreaElement {
     constructor() {
@@ -214,12 +126,4 @@ class CustomTextArea extends HTMLTextAreaElement {
 
 customElements.define('custom-text', CustomTextArea, { extends: 'textarea' });
 
-
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("/hub")
-    .withAutomaticReconnect([0, 0, 10000])
-    .build();
-connection.start().then(() => {
-    console.log(connection.connection.connectionId)
-}).catch(err => console.log(err.toString()));
 
