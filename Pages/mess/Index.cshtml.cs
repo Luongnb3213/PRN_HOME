41600d3f7c676e0006d32cb707174ba82c831158
Luongnb3213 <luongnb3213@gmail.com>
2024-06-19 18:06:20 +0700

    Add layout mess

---
+++ Pages/mess/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_Assignment.Pages.mess
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
